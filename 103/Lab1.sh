f95c527c976049b2d575c916c444904e37d64e8f
Andrew Covey <Andrew.D.Covey@Protonmail.com>
2025-05-08 14:21:01 -1000

    Commit @ VSCodium

---
+++ 103/Lab1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 01 Ops Challenge Solution
+# Author:  Andrew Covey
+# Date of latest revision:  5 / 8 / 2025
+# Purpose: Print a string to the terminal
+
+echo “Hello, world!”
+
+# This is here to keep git bash from just shutting the shell before anyone can see the output
+read -p "Press Enter to continue... " </dev/tty
