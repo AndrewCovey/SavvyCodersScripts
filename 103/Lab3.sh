5272d4dc3feb77d9cbf42cc32f593c1423bd7077
Andrew Covey <Andrew.D.Covey@Protonmail.com>
2025-05-12 13:53:23 -1000

    Commit @ VSCodium

--- 103/Lab3.sh
+++ 103/Lab3.sh
@@ -4,11 +4,11 @@ echo -n "Enter a number: "
 read anum
 
 if [ $anum -gt 5 ]; then
-  echo "your number is greater than 5"
+  echo "Your number is greater than 5"
 elif [ $anum -lt 5 ]; then
-  echo "your number is less than 5"
+  echo "Your number is less than 5"
 else
-  echo "your number is five."
+  echo "Your number is five."
 fi
 
 # This is here to keep git bash from insta-closing the powershell window before anyone can see the output...
