6a75747bb879b896a049e4013cf13632e4bf1116
Andrew Covey <Andrew.D.Covey@Protonmail.com>
2025-05-12 13:37:27 -1000

    Commit @ VSCodium

---
+++ 103/Lab3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -n "Enter a number: "
+read anum
+
+if [ $anum -gt 5 ]; then
+  echo "your number is greater than 5"
+elif [ $anum -lt 5 ]; then
+  echo "your number is less than 5"
+else
+  echo "your number is five."
+fi
+
+# This is here to keep git bash from just shutting the shell before anyone can see the output
+read -p "Press Enter to continue... " </dev/tty
