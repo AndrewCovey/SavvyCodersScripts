85f61236e2beaec3e39c3cc5c75cae54e71014ce
Andrew Covey <Andrew.D.Covey@Protonmail.com>
2025-05-12 17:03:23 -1000

    Commit @ VSCodium

--- 103/Lab4.sh
+++ 103/Lab4.sh
@@ -12,6 +12,5 @@ read moper
 let answer=$anum1$moper$anum2
 echo "The answer to $anum1 $moper $anum2 is: $answer"
 
-
 # This is here to keep git bash from insta-closing the powershell window before anyone can see the output...
 read -p "Press Enter to continue... " </dev/tty
