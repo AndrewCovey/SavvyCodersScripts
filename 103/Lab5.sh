72724f882d9cf1ea461328129940d7e1a6ab4125
Andrew Covey <Andrew.D.Covey@Protonmail.com>
2025-05-14 15:13:35 -1000

    Commit @ VSCodium

---
+++ 103/Lab5.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+do_report ($uname) {
+
+echo -en "Basic math script...  Enter two integers and a basic math operator.\nEnter an integer: "
+read anum1
+
+echo -n "Enter another integer: "
+read anum2
+
+echo -n "New enter a mathematical operator ('+', '-', '*', or '/'): "
+read moper
+
+let answer=$anum1$moper$anum2
+echo "The answer to $anum1 $moper $anum2 is: $answer"
+
+}
+
+do_math
+
+# This is here to keep git bash from insta-closing the powershell window before anyone can see the output...
+read -p "Press Enter to continue... " </dev/tty
