8b677a885d61d8a2b66ef0e89b977340cb105b38
Andrew Covey <Andrew.D.Covey@Protonmail.com>
2025-06-17 13:16:11 -1000

    Commit @ VSCodium

---
+++ Python/sleep_tracker.py
@@ -0,0 +1,8 @@
+#########################################################################################################################################################
+#  Tracks sleep, in a sorta not permenent way...                                                                                                        #
+#########################################################################################################################################################
+
+name = str(input("\nEnter your name please: "))
+hours = int(input("Enter your average hours of sleep per night: "))
+
+print ("\nHello, " + name + ".\nYou are getting about " + str(hours * 7) + " hours of sleep weekly.\n\nHave a nice day ... or night!")
