bccbe0d642c9d66fe02480a81bde144e10324a32
Andrew Covey <Andrew.D.Covey@Protonmail.com>
2025-07-14 23:44:15 +0000

    Commit @ VSCodium

--- Python/mini_calc.py
+++ Python/mini_calc.py
@@ -1,6 +1,8 @@
 def calc(num1, num2, oper):
     return eval(str(num1) + ' ' + str(oper) + ' ' + str(num2))
 
+# Let's try out our function interactive style!
+
 num1 = float(input('Provide a number: '))
 num2 = float(input('Provide another number: '))
 oper = str(input('Provide a four function arithmetic operator (+,-,*,/): '))
