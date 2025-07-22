b579290d864ca8a2551b2e40c7032c7a914bcb63
Andrew Covey <Andrew.D.Covey@Protonmail.com>
2025-07-22 00:25:29 +0000

    Commit @ VSCodium

--- Python/split_bill.py
+++ Python/split_bill.py
@@ -12,14 +12,14 @@
 
 #Write your code below this line 👇
 
-ppl = int(input("How many people are paying? "))
 cost = float(input("What was the total for the bill? $"))
 tip = int(input("What percent will you tip? "))
+ppl = int(input("How many people are paying? "))
 
 tot = (1 + (tip / 100)) * cost
 amt = round(tot/ppl,3)
 
-# or, harder to read ...
-amt =  round(((1 + (tip / 100)) * cost)/ppl,3)
+# or, if you want, and a little harder to read ...
+#amt =  round(((1 + (tip / 100)) * cost)/ppl,3)
 
 print("Each person should pay $" + str(amt))
