7669afa8252a5526c4a52db80fefaef64d822589
Andrew Covey <Andrew.D.Covey@Protonmail.com>
2025-07-29 23:42:17 +0000

    Commit @ VSCodium

---
+++ Python/websploit.py
@@ -0,0 +1,47 @@
+# This is a known websploit attack site to find info on a website follow the steps below to install the code and run it.
+# This script is not complete and is missing a few attacks but this was free on github.
+# A script kitty would find a script on github or metasploit or many other places and run it to attack devices with hardly any prior knowledge.
+# Features
+# You can find Ip Address of the website
+# You can do Ddos Attack on website
+# Bruteforce ( Instagram , Spotify , Twitter , Netfilx , Amazon Prime video , Pinrest ) [ Coming soon ]
+# Port Scanner [Coming soon]
+# Download
+
+
+# Linux :
+
+# Clone the Repository by using git clone command
+# git clone Webspoilt
+# git clone https://github.com/Deadshot0x7/Webspoilt.git
+# cd Webspoilt
+# pip install -r Requirments.txt or run setup.sh
+# python3 Webspoilt.py
+
+
+...
+
+Updated for July 2025
+
+Source information: https://github.com/f4rih/websploit, altered for use on a premade Kali VM (https://cdimage.kali.org/kali-2025.2/kali-linux-2025.2-virtualbox-amd64.7z)
+
+Installation
+
+Manual install via git :
+
+$ git clone https://github.com/f4rih/websploit.git
+$ cd websploit
+$ sudo python setup.py install
+
+Execute via command line :
+
+$ sudo websploit
+
+
+
+
+
+
+
+
+...
